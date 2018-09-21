13ff5bac64bdde004c190802e24c0ab230b91c77
Jens Eggerstedt <j.eggerstedt@kaibatech.de>
2018-09-21 12:04:59 +0200

    inject own postcss config before original config

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+function loadModule(src, filename) {
+  var Module = module.constructor;
+  var m = new Module();
+  m._compile(src, filename);
+  return m.exports;
+}
+
+function readPostcssConfig(generator) {
+  const filename = 'postcss.config.js';
+  const file = generator.files[filename];
+
+  if (file) {
+    const filePath = path.join(generator.context, filename);
+    fs.writeFileSync(filePath, '');
+    return loadModule(file, filename);
+  }
+
+  const config = generator.originalPkg.postcss;
+  if (config) {
+    const copy = { ...config };
+    delete config.plugins;
+    return copy;
+  }
+
+  return {};
+}
+
 module.exports = (api, options) => {
+  const postcss = readPostcssConfig(api.generator);
   const configs = {
     postcss: {
       plugins: {
@@ -7,13 +38,9 @@ module.exports = (api, options) => {
       },
     },
   };
-  api.addConfigTransform('postcss', {
-    file: {
-      js: ['postcss.config.js'],
-      json: ['.postcssrc.json', '.postcssrc'],
-      yaml: ['.postcssrc.yaml', '.postcssrc.yml'],
-    },
-  });
+
+  configs.postcss.plugins = { ...configs.postcss.plugins, ...postcss.plugins };
+
   api.extendPackage(configs);
 
   api.injectImports(api.entryFile, `import './assets/tailwind.css'`);
